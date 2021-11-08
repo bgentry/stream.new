4f0d5634a89a8b675772b09bb762d42eba5eaf28
Dylan Jhaveri <dylanjha@users.noreply.github.com>
2021-11-08 00:56:32 -0800

    use mux-elements/mux-video for playback

--- components/video-player.tsx
+++ components/video-player.tsx
@@ -1,12 +1,7 @@
 /* globals Image */
 import { useState, useEffect, useRef, forwardRef } from 'react';
-import Plyr from 'plyr';
-import 'plyr/dist/plyr.css';
-import Hls from 'hls.js';
-import mux from 'mux-embed';
 
-import logger from '../lib/logger';
-import { getStreamBaseUrl, getImageBaseUrl } from '../lib/urlutils'
+// import logger from '../lib/logger';
 import { breakpoints } from '../style-vars';
 import { HTMLVideoElementWithPlyr } from '../types';
 import { useCombinedRefs } from '../util/use-combined-refs';
@@ -49,9 +44,9 @@ type SizedEvent = {
 const VideoPlayer = forwardRef<HTMLVideoElementWithPlyr, Props>(({ playbackId, poster, currentTime, onLoaded, onError }, ref) => {
   const videoRef = useRef<HTMLVideoElementWithPlyr>(null);
   const metaRef = useCombinedRefs(ref, videoRef);
-  const playerRef = useRef<Plyr | null>(null);
+  // const playerRef = useRef<Plyr | null>(null);
   const [isVertical, setIsVertical] = useState<boolean | null>();
-  const [playerInitTime] = useState(Date.now());
+  // const [playerInitTime] = useState(Date.now());
 
   const videoError = (event: ErrorEvent) => onError(event);
 
@@ -78,63 +73,15 @@ const VideoPlayer = forwardRef<HTMLVideoElementWithPlyr, Props>(({ playbackId, p
 
   useEffect(() => {
     const video = videoRef.current;
-    const src = `${getStreamBaseUrl()}/${playbackId}.m3u8`;
-    let hls: Hls | null;
-    hls = null;
+    // let hls: Hls | null;
+    // hls = null;
     if (video) {
       video.addEventListener('error', videoError);
-      playerRef.current = new Plyr(video, {
-        previewThumbnails: { enabled: true, src: `${getImageBaseUrl()}/${playbackId}/storyboard.vtt` },
-        storage: { enabled: false },
-        fullscreen: {
-          iosNative: true
-        },
-        captions: { active: true, language: 'auto', update: true }
-      });
-
-      if (video.canPlayType('application/vnd.apple.mpegurl')) {
-        // This will run in safari, where HLS is supported natively
-        video.src = src;
-      } else if (Hls.isSupported()) {
-        // This will run in all other modern browsers
-        hls = new Hls();
-        hls.loadSource(src);
-        hls.attachMedia(video);
-        hls.on(Hls.Events.ERROR, function (event, data) {
-          if (data.fatal) {
-            logger.error('hls.js fatal error');
-            videoError(new ErrorEvent('HLS.js fatal error'));
-          }
-        });
-      } else {
-        console.error( // eslint-disable-line no-console
-          'This is an old browser that does not support MSE https://developer.mozilla.org/en-US/docs/Web/API/Media_Source_Extensions_API',
-        );
-      }
-
-      if (typeof mux !== 'undefined' && process.env.NEXT_PUBLIC_MUX_ENV_KEY) {
-        mux.monitor(video, {
-          hlsjs: hls,
-          Hls,
-          data: {
-            env_key: process.env.NEXT_PUBLIC_MUX_ENV_KEY,
-
-            player_name: 'hls.js player v1',
-            video_id: playbackId,
-            video_title: playbackId,
-            video_stream_type: 'on-demand',
-            player_init_time: playerInitTime,
-          },
-        });
-      }
     }
 
     return () => {
       if (video) {
         video.removeEventListener('error', videoError);
-      }
-      if (hls) {
-        hls.destroy();
       }
     };
   }, [playbackId, videoRef]);
@@ -146,10 +93,23 @@ const VideoPlayer = forwardRef<HTMLVideoElementWithPlyr, Props>(({ playbackId, p
     }
   }, [currentTime]);
 
+  useEffect(() => {
+    import('@mux-elements/mux-video');
+  }, []);
+
   return (
     <>
       <div className='video-container'>
-        <video ref={metaRef} poster={poster} controls playsInline />
+        <mux-video
+          ref={metaRef}
+          env-key={process.env.NEXT_PUBLIC_MUX_ENV_KEY}
+          playback-id={playbackId}
+          poster={poster}
+          metadata-video-title={playbackId}
+          stream-type="vod"
+          controls
+          playsInline
+        />
       </div>
       <style jsx>{`
         :global(:root) {
